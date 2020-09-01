ab5fa6037958fb60dc2b7caf498397382fd810f2
root <root@ip-172-31-33-163.us-east-2.compute.internal>
2020-09-01 06:19:26 +0000

    remove server.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from pymongo import MongoClient
 import datetime
 from flask import Flask, render_template, request
@@ -8,36 +8,39 @@ from bson.json_util import dumps
 
 app = Flask(__name__)
 
-client = MongoClient() #mongodb와 연결
+client = MongoClient('mongodb://admin:vision1234@localhost:27017/') #mongodb와 연결
 db = client.mapdata #db이름 mapdata로 db 호출
 collection = db.info #collection 이름 info를 호출
 
 @app.route('/', methods=['GET', 'POST'])
-def hello_world():
+def hello_vision():
+    # print(request.json)
+
+    # return dict({"hello":"hello"})
     return render_template('index.html')
 
-#python script 따로
-@app.route('/read',methods=['GET', 'POST'])
-def mongoRead():
-    # client = MongoClient('mongodb://admin:vision1234@localhost:27017/')
-    results = collection.find() #json 통째로 보여주기
-    client.close()
-    temp = "["
-    for i in results:
-        i.pop('_id',None)
-        i.pop('neighbor_sectors_id',None)
-        temp += json.dumps(i)
-        temp += ", "
-    temp=temp[:-2]
-    temp += "]"
-    # print(type(temp))
-    # print(temp)
-    # jl = json.loads(temp)    
-    # print(type(jl))
-    # print(jl)
-    # print(tuple(jl))
-    return temp
-    #return render_template('readdb.html', data=results)
+@app.route('/mapdata/<station>',methods=['GET', 'POST'])
+def mongoRead(station):
+    print(station)
+    return "sss"
+    # results = collection.find() #json 통째로 보여주기
+    # client.close()
+    # temp = "["
+    # for i in results:
+    #     i.pop('_id',None)
+    #     i.pop('neighbor_sectors_id',None)
+    #     temp += json.dumps(i)
+    #     temp += ", "
+    # temp=temp[:-2]
+    # temp += "]"
+    # # print(type(temp))
+    # # print(temp)
+    # # jl = json.loads(temp)    
+    # # print(type(jl))
+    # # print(jl)
+    # # print(tuple(jl))
+    # return temp
+    # #return render_template('readdb.html', data=results)
 
 @app.route('/remove', methods=['GET'])
 def mongoDelete():
